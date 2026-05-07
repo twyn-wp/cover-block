5cd6e21078267fc05ab4b93a75bc487ef3ad2f9b
Tony Nguyen <tonngu137@gmail.com>
2026-05-07 07:51:58 +0200

    feat: add helper text constant

--- src/cover/constants.js
+++ src/cover/constants.js
@@ -1,6 +1,7 @@
 /**
  * WordPress dependencies
  */
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
@@ -8,18 +9,72 @@
 
 import metadata from './block.json';
 
-export const TAG_NAME_OPTIONS = metadata.attributes.tagName.enum.map(
-	( tag ) => {
-		// Add "Default" to the label for the default div tag (same behavior as core/group)
-		if ( tag === 'div' ) {
-			tag = `Default (<${ tag }>)`;
-		} else {
-			tag = `<${ tag }>`;
+// Use the block metadata enum as the available tag name options.
+const tagNames = metadata.attributes.tagName.enum;
+
+export const TAG_NAME_OPTIONS = tagNames.map( ( tag ) => {
+	// Add "Default" to the label for the default div tag (same behavior as core/group)
+	if ( tag === 'div' ) {
+		tag = `Default (<${ tag }>)`;
+	} else {
+		tag = `<${ tag }>`;
+	}
+
+	return {
+		label: tag,
+		value: tag,
+	};
+} );
+
+// Same helper text as the core/group block.
+export const TAG_HELP_TEXT = Object.fromEntries(
+	tagNames.map( ( key ) => {
+		let text = '';
+
+		switch ( key ) {
+			case 'div':
+				text = __(
+					'The <div> element should only be used if the block is a design element with no semantic meaning.',
+					'twyn-cover-block'
+				);
+				break;
+			case 'header':
+				text = __(
+					'The <header> element should represent introductory content, typically a group of introductory or navigational aids.',
+					'twyn-cover-block'
+				);
+				break;
+			case 'main':
+				text = __(
+					'The <main> element should be used for the primary content of your document only.',
+					'twyn-cover-block'
+				);
+				break;
+
+			case 'section':
+				text = __(
+					"The <section> element should represent a standalone portion of the document that can't be better represented by another element.",
+					'twyn-cover-block'
+				);
+				break;
+
+			case 'article':
+				text = __(
+					"The <aside> element should represent a portion of a document whose content is only indirectly related to the document's main content.",
+					'twyn-cover-block'
+				);
+				break;
+
+			case 'footer':
+				text = __(
+					'The <footer> element should represent a footer for its nearest sectioning element (e.g.: <section>, <article>, <main> etc.).',
+					'twyn-cover-block'
+				);
+				break;
+			default:
+				break;
 		}
 
-		return {
-			label: tag,
-			value: tag,
-		};
-	}
+		return [ key, text ];
+	} )
 );
