538589ee1a5c2741ee3b1d10154a6ba821c49557
Tony Nguyen <tonngu137@gmail.com>
2026-05-08 07:09:42 +0200

    feat(edit): add HTML tag selector UI to advanced InspectorControls

--- src/cover/edit.js
+++ src/cover/edit.js
@@ -2,18 +2,43 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { useBlockProps } from '@wordpress/block-editor';
+import {
+	useBlockProps,
+	InspectorAdvancedControls,
+} from '@wordpress/block-editor';
+import { SelectControl } from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
-
 import './editor.scss';
+import { TAG_NAME_OPTIONS, TAG_HELP_TEXT } from './constants.js';
 
-export default function Edit() {
+export default function Edit( { attributes, setAttributes } ) {
+	// * Attributes
+	const { tagName } = attributes;
+
+	// * Handlers
+	const onChangeTagName = ( newTagName ) => {
+		setAttributes( { tagName: newTagName } );
+	};
+
 	return (
-		<p { ...useBlockProps() }>
-			{ __( 'Hello from edit.js', 'twyn-cover-block' ) }
-		</p>
+		<>
+			<InspectorAdvancedControls>
+				<SelectControl
+					__next40pxDefaultSize
+					__nextHasNoMarginBottom="true"
+					label="HTML-Element"
+					value={ tagName }
+					options={ TAG_NAME_OPTIONS }
+					onChange={ onChangeTagName }
+					help={ TAG_HELP_TEXT[ tagName ] }
+				/>
+			</InspectorAdvancedControls>
+			<tagName { ...useBlockProps() }>
+				{ __( 'Hello from edit.js', 'twyn-cover-block' ) }
+			</tagName>
+		</>
 	);
 }
