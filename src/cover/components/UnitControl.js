e722c590214a94155140932a414cd94c17e0a660
Tonyトニイ <tonngu137@gmail.com>
2026-05-19 11:53:10 +0200

    chore: add unit control adapter

---
+++ src/cover/components/UnitControl.js
@@ -0,0 +1,7 @@
+/**
+ * WordPress dependencies
+ */
+import * as components from '@wordpress/components';
+
+export const UnitControl =
+	components.UnitControl || components.__experimentalUnitControl;
