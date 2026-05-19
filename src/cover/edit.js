2106d7811559a6529b828c6e5fc21dbac36bb103
Tonyトニイ <tonngu137@gmail.com>
2026-05-19 12:16:10 +0200

    feat: add min height editor control

--- src/cover/edit.js
+++ src/cover/edit.js
@@ -4,6 +4,7 @@
 import { __ } from '@wordpress/i18n';
 import {
 	useBlockProps,
+	InspectorControls,
 	InspectorAdvancedControls,
 } from '@wordpress/block-editor';
 import { SelectControl } from '@wordpress/components';
@@ -12,25 +13,61 @@ import { SelectControl } from '@wordpress/components';
  * Internal dependencies
  */
 import './editor.scss';
-import { TAG_NAME_OPTIONS, TAG_HELP_TEXT } from './constants.js';
+import { UnitControl } from './components/UnitControl.js';
+import {
+	TAG_NAME_OPTIONS,
+	TAG_HELP_TEXT,
+	DEFAULT_MIN_HEIGHT,
+	MIN_HEIGHT_VAR,
+	MIN_HEIGHT_UNITS,
+} from './constants.js';
+import { getStyleObjectIfNotDefault } from './utils.js';
 
 export default function Edit( { attributes, setAttributes } ) {
 	// * Attributes
-	const { tagName } = attributes;
+	const { tagName, minHeight } = attributes;
 
 	// * Derived State
 	const TagName = tagName;
 
 	// * Block Props
-	const blockProps = useBlockProps();
+	const style = {
+		...getStyleObjectIfNotDefault(
+			MIN_HEIGHT_VAR,
+			minHeight,
+			DEFAULT_MIN_HEIGHT
+		),
+	};
+
+	const blockProps = useBlockProps( {
+		style,
+	} );
 
 	// * Handlers
 	const onChangeTagName = ( newTagName ) => {
 		setAttributes( { tagName: newTagName } );
 	};
 
+	const onChangeMinHeight = ( newValue ) => {
+		setAttributes( { minHeight: newValue } );
+	};
+
 	return (
 		<>
+			<InspectorControls group="dimensions">
+				<div style={ { gridColumn: '1/-1' } }>
+					<UnitControl
+						label={ __(
+							'min Height (Default: 250px)',
+							'twyn-cover-block'
+						) }
+						__next40pxDefaultSize
+						onChange={ onChangeMinHeight }
+						value={ minHeight }
+						units={ MIN_HEIGHT_UNITS }
+					/>
+				</div>
+			</InspectorControls>
 			<InspectorAdvancedControls>
 				<SelectControl
 					__next40pxDefaultSize
